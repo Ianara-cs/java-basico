aa9212c9445af9fa12a21a7b4c33f27691388d8e
Ianara-cs <ianarasilva333@gmail.com>
2023-08-25 18:03:25 -0300

    implementando a lógica

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome!");
+        String nome = scanner.next();
+        System.out.println("Por favor, digite o numero da agencia!");
+        String agencia = scanner.next();
+        System.out.println("Por favor, digite o numero da conta!");
+        int numero = scanner.nextInt();
+        System.out.println("Por favor, digite o saldo da conta!");
+        double saldo = scanner.nextDouble();
+
+        System.out.println(
+                "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta "
+                        + numero + " e seu saldo R$" + saldo + " já esta disponivel para saque.");
+
     }
 }
